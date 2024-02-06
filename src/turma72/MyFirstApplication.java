aab472e1653abdd1b09c7a71c5fd5e60ab0313c0
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-02-06 11:59:42 -0300

    Transferência da main para o helloworld

--- src/turma72/MyFirstApplication.java
+++ src/turma72/MyFirstApplication.java
@@ -2,12 +2,15 @@ package turma72;
 
 public class MyFirstApplication {
 
-	public static void main(String[] args) { //public = público; static é fixo, void = não retorna um valor
+	public static void main(String[] args) {
+		//public = público; static é fixo, void = não retorna um valor
 		// TODO Auto-generated method stub
 		String nome;
+		Integer idade;
 		nome = "Gabriel";
+		idade = 1;
 		
-		System.out.println("Hello World, essa é a primeira aplicação de " + nome);
+		System.out.println("Hello World, essa é a primeira aplicação de " + nome + " " + idade);
 	}
 
 }
