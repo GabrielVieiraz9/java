66608625767dd6541bf0c10da8d0f25152db096f
Gabriel Vieira <147213232+GabrielVieiraz9@users.noreply.github.com>
2024-02-06 16:19:18 -0300

    Revert "Adição da idade no código"

--- src/turma72/MyFirstApplication.java
+++ src/turma72/MyFirstApplication.java
@@ -2,15 +2,12 @@ package turma72;
 
 public class MyFirstApplication {
 
-	public static void main(String[] args) {
-		//public = público; static é fixo, void = não retorna um valor
+	public static void main(String[] args) { //public = público; static é fixo, void = não retorna um valor
 		// TODO Auto-generated method stub
 		String nome;
-		Integer idade;
 		nome = "Gabriel";
-		idade = 1;
 		
-		System.out.println("Hello World, essa é a primeira aplicação de " + nome + " " + idade);
+		System.out.println("Hello World, essa é a primeira aplicação de " + nome);
 	}
 
 }
